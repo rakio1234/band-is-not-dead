59da467a29940b8fe174a367e885e9eb3639f504
uchida yuta <yuta.uchida0101@gmail.com>
2015-12-21 21:26:52 +0900

    レイアウトの調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  private
+
+  def parse_as_html(text)
+    return if text.blank?
+
+    html = escape_as_html(text)
+    html = parse_newline_as_html(html)
+
+    html
+  end
+
+  def escape_as_html(text)
+    ERB::Util.html_escape(text)
+  end
+
+  def parse_newline_as_html(html)
+    html.gsub(/\r\n|\r|\n/, "<br>").html_safe
+  end
 end
