3768341e3ed59a34ae97a14626e547ed2503da22
uchida yuta <yuta.uchida0101@gmail.com>
2016-01-02 15:59:19 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :name, presence: true, length: { maximum: 50 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221123725) do
+ActiveRecord::Schema.define(version: 20151221125529) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name",        limit: 255,   null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20151221123725) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255,              null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
