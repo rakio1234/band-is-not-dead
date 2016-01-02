0c78e3bf02051ba74b6535425334318da1c2ad0c
uchida yuta <yuta.uchida0101@gmail.com>
2016-01-02 21:45:52 +0900

    ジャンル検索ができる

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,9 @@ class SearchController < ApplicationController
   # GET /serarch
   def index
     @genres = Band.tag_counts_on(:genres).order('count DESC')
+
+    if params[:id].present?
+      @bands = Band.tagged_with(params[:id])
+    end
   end
 end
